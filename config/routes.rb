b7f9f99eb5e47321c75d8f3268e9ff84844339d5
Michael Zhu <zlymike@outlook.com>
2017-07-07 02:34:37 +0000

    added user authtentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
+
   end
 
   resources :orders, only: [:create, :show]
@@ -17,6 +18,12 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
