d93dded01a456a15bdab93edbb4ca52d4bb630f6
Michael Zhu <zlymike@outlook.com>
2017-07-10 05:57:34 +0000

    fixed security for admin access

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CategoriesController < ApplicationController
-  before_filter :authorize
-  
+
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+
   def index
     @category = Category.all
   end
